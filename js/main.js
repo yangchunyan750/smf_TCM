9833b2eb76e8c7e512160be052fd2585b1547da7
yangchunyan <https://github.com/echo750>
2020-02-17 15:52:37 +0800

    1、导航栏横向显示  2、增加基本信息的必填项  3、颜色调整

--- js/main.js
+++ js/main.js
@@ -10,28 +10,43 @@ $(document).ready(function(){
   }).mouseover(function(){
   	  $(this).css('cursor','pointer');
   })
-  //主模块点击改变背景色并切换右侧显示
-  $('#content li').click(function(){
+  //主模块点击改变背景色并切换显示
+  $('.nav li').click(function(){
 	  $('.content_wrap').css('display','none');
-	  $('#content li').css({'background-color':'rgb(243,243,243)','color':'#000'});
-	  $(this).css({'background-color':'rgb(31,184,205)','color':'#fff'});
-	  $('.'+$(this).attr("class")).css('display','block')
+	  $('.nav li').css({'color':'#000'});
+	  $(this).css({'color':'rgb(15,156,237)'});
+	  // $('.' + $(this).attr("class") +'> .line').css({'display':'block','left':'40%'});
+	  $('#content .'+$(this).attr("class")).css('display','block');
   })
   //点击下一步切换到下个模块界面
-  $("#content-right .next_btn").click(function(){
+  $("#content-page .next_btn").click(function(){
 	  var page = '.'+$(this).parent().attr("id");
 	  var num = parseInt(page.charAt(page.length-1));
-	  if("" != $("#name").val() || "" != $("#sex").val() || $("#sex").val()){
+	  // 姓名,出生日期,年龄为空时弹出对话框
+	  if("" != $("#name").val().trim() && "" != $("#date").val().trim() && "" != $("#age").val().trim()){
 		  $('#page'+(num)).css('display','none');
 		  $('#page'+(num+1)).css('display','block');
-		  $('#content-left ul li').css({'background-color':'rgb(243,243,243)','color':'#000'});
-		  $('#content-left ul li').eq(num).css({'background-color':'rgb(31,184,205)','color':'#fff'})
+		  $('.nav li').css({'color':'#000'});
+		  $('.nav li').eq(num).css({'color':'rgb(15,156,237)'})
 	  }else{
-		  if("" == $("#name").val().trim()){
-			  alert("请输入姓名")
+		  if("" == $("#name").val()){
+			  myAlert('','请输入姓名！',function(){});  
+		  }
+		  if("" == $("#date").val()){
+			  myAlert('','请选择出生日期！',function(){});  
+		  }
+		  if("" == $("#age").val()){
+		  	  myAlert('','请输入年龄！',function(){});  
 		  }
 	  }
 	  
+  })
+  // 点击年龄,根据前面的出生日期进行判断
+  $('#age').click(function(){
+	  if("" != $("#date").val().trim()){
+		  var year = $("#date").val().trim().split('-')[0];
+		  $("#age").val(parseInt(new Date().getFullYear()) - parseInt(year));
+	  }
   })
   //追加问诊题目选项
   var testCheckBox = 0;
