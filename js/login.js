08db04dfb706d53f5dbc04e8d717978cdf78f156
yangchunyan <412635804@qq.com>
2020-06-04 15:47:45 +0800

    6.4修改

--- js/login.js
+++ js/login.js
@@ -1,56 +1,17 @@
 $(function(){
+	//触发键盘回车登录事件
+	$('#password').keyup(function(event){
+		 if(event.keyCode ==13){
+			var data = {
+				  userName: $("#userName").val(),
+				  password: $("#password").val(),
+			 };
+			login($(this));
+		 }
+	});
 	//点击登录按钮发送异步请求
 	$(".btn").click(function(){
-		var data = {
-			  userName: $("#userName").val(),
-			  password: $("#password").val(),
-		 };
-		 // 点击登录按钮判空提示
-		 if(data.userName == "" || data.userName == null){
-			 $(".msg").html("请输入账号");
-			 return;
-		 }
-		 if(data.password == "" || data.password == null){
-			 $(".msg").html("请输入密码");
-			 return;
-		 }
-		$(this).text('登录中...');
-		yAjax.post('../php/login.php',data,function(res){
-			 if(res <= 0){
-				 setTimeout(function(){
-					$("#password").focus();
-				},1000)
-				$(".btn").text('登录');
-				$('.msg').html('用户名或密码输入不正确,请重新输入');
-			 }else{
-				 $('#Popup').hide();
-				  yCookie.set("userName",data.userName);
-				 var userName = data.userName;
-				 if (userName != null && userName != '' && userName != undefined) {
-					 $(".leftNav li").removeClass('active');
-					 var infoNav ="<li class='active' page='page0' url='pages/info.html'><span class='bg1'></span><p>基本信息</p></li>";
-					 $('.leftNav ul').prepend(infoNav);
-				 	var logout = "<span>" + userName + "</span>&nbsp;&nbsp;<span onclick='toLogout();' class='text-center' id='exit'>|&nbsp;&nbsp;退出</span>";
-				 	$(".login").empty().html(logout);
-					$('.wrap').load("../pages/info.html");
-					$(".leftNav li").click(function(){
-						// if($('#name').val() != '' && $('#name').val() != undefined && $('#name').val() != null){
-						// 	localStorage.setItem("name",$('#name').val());
-						// }
-						// if($('#sec').val() != '' && $('#sex').val() != undefined && $('#sex').val() != null){
-						// 	localStorage.setItem("sex",$('#sex').val());
-						// }
-						//循环所有的input输入框并把该值存到json中
-						$(".leftNav li").removeClass("active");
-						$(this).addClass("active");
-						var loadUrl = $(this).attr("url");
-						var index = $(this).index();//获取ul的第几个li
-						yCookie.set("currPageIndex", index);
-						$('.wrap').load(baseUrl.webProjUrl() + loadUrl);
-					})
-				 }
-			 }
-		 })
+		login($(this));
 	})
 	//默认游客方式
 	chkLogin();
@@ -95,4 +56,51 @@ function toLogout () {
 			localStorage.clear();
 		}
 	})
+}
+
+function login(that){
+	var data = {
+		  userName: $("#userName").val(),
+		  password: $("#password").val(),
+	 };
+	 // 点击登录按钮判空提示
+	 if(data.userName == "" || data.userName == null){
+		 $(".msg").html("请输入账号");
+		 return;
+	 }
+	 if(data.password == "" || data.password == null){
+		 $(".msg").html("请输入密码");
+		 return;
+	 }
+	that.text('登录中...');
+	yAjax.post('http://localhost:8080/smf_TCM_back2/login.php',data,function(res){
+		 if(res <= 0){
+			 setTimeout(function(){
+				$("#password").focus();
+			},1000)
+			$(".btn").text('登录');
+			$('.msg').html('用户名或密码输入不正确,请重新输入');
+		 }else{
+			 $('#Popup').hide();
+			  yCookie.set("userName",data.userName);
+			 var userName = data.userName;
+			 if (userName != null && userName != '' && userName != undefined) {
+				 $(".leftNav li").removeClass('active');
+				 var infoNav ="<li class='active' page='page0' url='pages/info.html'><span class='bg1'></span><p>基本信息</p></li>";
+				 $('.leftNav ul').prepend(infoNav);
+			 	var logout = "<span>" + userName + "</span>&nbsp;&nbsp;<span onclick='toLogout();' class='text-center' id='exit'>|&nbsp;&nbsp;退出</span>";
+			 	$(".login").empty().html(logout);
+				$('.wrap').load("../pages/info.html");
+				$(".leftNav li").click(function(){
+					//循环所有的input输入框并把该值存到json中
+					$(".leftNav li").removeClass("active");
+					that.addClass("active");
+					var loadUrl = that.attr("url");
+					var index = that.index();//获取ul的第几个li
+					yCookie.set("currPageIndex", index);
+					$('.wrap').load(baseUrl.webProjUrl() + loadUrl);
+				})
+			 }
+		 }
+	 })
 }
--- js/main.js
+++ js/main.js
@@ -33,6 +33,32 @@ $(document).ready(function(){
 		  nextBtn($(this));
 	  }
   })
+  
+  $(".page3 .next_btn").click(function(){
+	  if($('#con_type').val() == null || $('#con_type').val() == '' || $('#con_type').val() == undefined){
+		  $.alerts.alert('提示','类型不能为空',function(){
+			  return false;
+		  });
+	  }else if($('#dia_result').val() == null || $('#dia_result').val() == '' || $('#dia_result').val() == undefined){
+		  $.alerts.alert('提示','辩证结果不能为空',function(){
+			  return false;
+		  });
+	  }else if($('#xzzknbx').val() == null || $('#xzzknbx').val() == '' || $('#xzzknbx').val() == undefined){
+		  $.alerts.alert('提示','可能表现的症状不能为空',function(){
+			  return false;
+		  });
+	  }else if($('#fbqx').val() == null || $('#fbqx').val() == '' || $('#fbqx').val() == undefined){
+		  $.alerts.alert('提示','发病倾向不能为空',function(){
+			  return false;
+		  });
+	  }else if($('#zjjy').val() == null || $('#zjjy').val() == '' || $('#zjjy').val() == undefined){
+		  $.alerts.alert('提示','专家建议不能为空',function(){
+			  return false;
+		  });
+	  }else{
+	  		  nextBtn($(this));
+	  }
+  })
   // 点击年龄,根据前面的出生日期进行判断
   $('#age').click(function(){
 	  if("" != $("#date").val().trim()){
@@ -40,7 +66,7 @@ $(document).ready(function(){
 		  $("#age").val(parseInt(new Date().getFullYear()) - parseInt(year));
 	  }
   })
-  
+ 
 	function nextBtn(that){
 		var page = '.'+that.parent().attr("page");
 		$(".leftNav li").removeClass("active");
